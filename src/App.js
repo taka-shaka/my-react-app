160a23eb5ebfcf3003f5d99a5bb9266934dc223d
taka-shaka <shaka.taka.0608@gmail.com>
2025-10-17 07:51:30 +0900

    App.js を編集しました

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>こんにちは！Reactアプリが動いています🎉</h1>
+      <p>これはVercelで公開された私の最初のReactアプリです。</p>
     </div>
   );
 }
